0d8d98579ea224fb65b55e21ec8afeca67dafbbc
doderick <doderick@outlook.com>
2018-09-15 11:30:10 +0000

    注册与登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,8 +16,33 @@
 
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">登录</a></li>
-                <li><a href="#">注册</a></li>
+                {{-- 游客用户访问登录和注册 --}}
+                @guest
+                    <li><a href="{{ route('login') }}">登录</a></li>
+                    <li><a href="{{ route('register') }}">注册</a></li>
+                {{-- 登录用户显示下拉菜单 --}}
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropsown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="user-avatar pull-left" style="margin-right: 8px;margin-top: -5px;">
+                                <img src="/images/avatar_1.png" class="img-responsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                    退出登录
+                                </a>
+                                <form action="{{ route('logout') }}" method="post" id="logout-form" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
