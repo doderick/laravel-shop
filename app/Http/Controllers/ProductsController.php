a5e16af5e989f59cddb6c1562ac5340c5505748a
doderick <doderick@outlook.com>
2018-09-19 05:47:27 +0000

    用户界面 - 收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -116,4 +116,17 @@ class ProductsController extends Controller
 
         return;
     }
+
+    /**
+     * 展示收藏商品列表
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function favorites(Request $request)
+    {
+        $products = $request->user()->favoriteProducts()->paginate(16);
+
+        return view('products.favorites', ['products' => $products]);
+    }
 }
